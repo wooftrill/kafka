10ae5b40c2bc8a6be35a87584c99ffae2e4874f0
woofandtrill <woofandtrill@gmail.com>
2023-09-11 07:50:06 -0700

    kafka producer

--- kafka_producer/config/Config.py
+++ kafka_producer/config/Config.py
@@ -1,5 +1,5 @@
 import os
 
-KAFKA = {"topic":"wt-topic-0","address": "34.16.156.238:9092"}
+KAFKA = {"topic":"wt-topic-0","address": "localhost:9092"}
 
 TABLE = {"checkout": "tbl_checkout", "metadata": "tbl_payment_metadata_producer","user_session_cart": "tbl_user_session_cart"}
